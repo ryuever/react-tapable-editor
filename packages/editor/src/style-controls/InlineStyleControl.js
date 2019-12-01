9a47fa97fcb74659070ece7238e689dab6f7c80d
youchao liu <ryuforever@163.com>
2019-12-01 09:04:50 +0800

    fix inline style when typing chinese

--- packages/editor/src/style-controls/InlineStyleControl.js
+++ packages/editor/src/style-controls/InlineStyleControl.js
@@ -8,8 +8,29 @@ var INLINE_STYLES = [
   {label: 'Monospace', style: 'CODE'},
 ];
 
+// 1. 当切换到输入中文的时候，如果说这个时候点击`inline style button`的话，
+//    它是不会生效到下一个中文字符的
+// 2. 当换行的时候，上一个block最后字符的`inline-style`无法自动应用到新的一行
+//    （新的一行如果说是输入中文）
+
 export default ({ editorState, onToggle }) => {
-  const currentStyle = editorState.getCurrentInlineStyle();
+  const selection = editorState.getSelection()
+  const contentState = editorState.getCurrentContent()
+  let currentStyle = editorState.getCurrentInlineStyle();
+
+  console.log('curent : ', currentStyle)
+
+  // 主要是为了解决当输入中文的时候，会出现`active inline style`被清空的现象；
+  if (!currentStyle.size && selection.isCollapsed()) {
+    const block = contentState.getBlockForKey(selection.getAnchorKey())
+    const startOffset = selection.getStartOffset();
+    const chars = block.getCharacterList()
+    const length = chars.size
+
+    if (length < startOffset) {
+      currentStyle = block.getInlineStyleAt(length - 1)
+    }
+  }
 
   return (
     <div className="RichEditor-inline-controls">
