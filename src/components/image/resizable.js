f1cbc90d0c8c1163e8da97a671c61164fd3aa634
youchao liu <ryuforever@163.com>
2020-01-21 00:51:48 +0800

    add `resizable` docs

---
+++ src/components/image/resizable.js
@@ -0,0 +1,5 @@
+// https://techcrunch.com/2019/12/02/why-notion-is-staying-small-as-its-valuation-gets-bigger/
+// imageToolbar是决定它的相对位置；同时会影响到它进行resize时的可选择方向
+// 1. 如果说当前是 align-center的话，resize的时候要确保一直居中
+// 2. 如果说当前是 align-left的话，只能够向左
+// 3. 如果说当前是 align-right的话，只能够向右
