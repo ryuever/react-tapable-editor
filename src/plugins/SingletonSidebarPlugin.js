e957e77acb01014798f1028d9d8cbffa7cf4f0c0
youchao liu <ryuforever@163.com>
2020-03-22 23:52:04 +0800

    create sidebar addon on every update

--- src/plugins/SingletonSidebarPlugin.js
+++ src/plugins/SingletonSidebarPlugin.js
@@ -2,13 +2,34 @@ import { EditorState } from "draft-js";
 import throttle from "../utils/throttle";
 import getBoundingRectWithSafeArea from "../utils/rect/getBoundingRectWithSafeArea";
 import findBlockContainsPoint from "../utils/rect/findBlockContainsPoint";
-import getRootNode from "../utils/rect/getRootNode";
+import createAddOn from "./sidebar-plugin/createAddOn";
+import { getNodeByOffsetKey } from "../utils/findNode";
+import { getSelectableNodeByListenerKey } from "./sidebar-plugin/utils";
+import "./sidebar-plugin/styles.css";
+
+/**
+ * The reason why sidebar should be created every times...
+ * 1. An attempt to create a top level Sidebar component, but when fulfill drag functionality,
+ *    It has big trouble...How could i set `draggable` attribute and drag the related block.
+ */
 
 function SingletonSidebarPlugin() {
-  let currentBlockInfoWithSidebar = null;
+  let current = null;
 
   this.apply = getEditor => {
     const { hooks, editorRef, sidebarRef } = getEditor();
+
+    const removeNode = () => {
+      try {
+        if (!current) return;
+        const { node, child } = current;
+        // check has child first.
+        if (node.contains(child)) node.removeChild(child);
+        current = null;
+      } catch (err) {
+        console.log("err ", err);
+      }
+    };
 
     const mouseMoveHandler = e => {
       const { editorState } = getEditor();
@@ -19,27 +40,24 @@ function SingletonSidebarPlugin() {
       const nodeInfo = findBlockContainsPoint(coordinateMap, { x, y });
 
       if (nodeInfo) {
-        const { rect } = nodeInfo;
-        currentBlockInfoWithSidebar = nodeInfo;
-        const { top, left } = rect;
-
-        const rootNode = getRootNode(editorRef);
-        const rootRect = rootNode.getBoundingClientRect();
-        const rootOffsetTop = rootNode.offsetTop;
-        const rootOffsetLeft = rootNode.offsetLeft;
-
-        const nextTop = rootOffsetTop + top - rootRect.top;
-        const nextLeft = left + 100 - 50;
-
-        sidebarRef.current.style.opacity = 1;
-        sidebarRef.current.style.top = `${nextTop}px`;
-        sidebarRef.current.style.left = `${nextLeft}px`;
+        const { key } = nodeInfo;
+        const node = getNodeByOffsetKey(key);
+        if (current) {
+          if (current.node === node) return;
+          else removeNode();
+        }
+        const child = createAddOn(key);
+        node.appendChild(child);
+        const selectableNode = getSelectableNodeByListenerKey(key);
+        // https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
+        requestAnimationFrame(() => {
+          child.classList.add("sidebar-addon-visible");
+        });
+        current = { node, child, key };
       }
     };
     const keyDownHandler = e => {
-      if (!currentBlockInfoWithSidebar) return;
-      sidebarRef.current.style.opacity = 0;
-      currentBlockInfoWithSidebar = null;
+      removeNode();
     };
 
     const throttledMoveHandler = throttle(mouseMoveHandler, 50);
